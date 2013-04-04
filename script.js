68357f08768bb7eb8699e1d07422ee3c2138e51b
rentzsch <jwr.git@redshed.net>
2013-04-03 19:49:08 -0500

    redirect to github oauth

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	$('#sign-in-button').click(function(){
-		console.log('sign in clicked');
+		window.location = 'https://github.com/login/oauth/authorize?client_id=17d941ffa650bd861e83&scope=gist';
 	});
 });
